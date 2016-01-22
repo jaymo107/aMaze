0d94e71113e2b1f2aba3fb6994345dbbec6d9ba7
Kiran <kirancaudrey@yahoo.co.uk>
2016-01-22 13:56:39 +0000

    Added Block Class

---
+++ com/amaze/main/Block.java
@@ -0,0 +1,15 @@
+package com.amaze.main;
+
+/**
+ * Created by Kiran on 22/01/2016.
+ */
+public abstract class Block {
+    /*
+    @todo:
+        - Collision detection
+        - Image handler
+        - Current location
+        - Physical Size e.g. 10x10px NOT HOW MANY BLOCKS IT TAKES
+     */
+
+}
