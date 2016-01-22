105dd4883ae042b15a704c1263d962c4047782db
Pasha <pavelsdembo@gmail.com>
2016-01-22 18:56:39 +0000

    created constructor and added some basic functions

--- com/amaze/main/Menu.java
+++ com/amaze/main/Menu.java
@@ -1,13 +1,36 @@
 package com.amaze.main;
+import org.jsfml.graphics.*;
 
 /**
  * This class will handle the menu and buttons associated with it.
  */
 public class Menu {
 
-    private Menu() {
+    private final int NUMBER_OF_ITEMS = 5; // Number of items available to select in the menu
+
+    public Menu() {
 
 
     }
 
+    /**
+     * This function will handle an event when user presses arrow key up
+     */
+    public void arrowKeyUp() {
+
+    }
+
+    /**
+     * This function will handle an event when user presses arrow key down
+     */
+    public void arrowKeyDown() {
+
+    }
+
+    /**
+     * This function will handle an event when user presses an enter key
+     */
+    public void enterPressed() {
+
+    }
 }
