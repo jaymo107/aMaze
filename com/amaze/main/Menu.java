22959594452cf9c1a5ed3ba0fc812fd52e76368d
Kiran <kirancaudrey@yahoo.co.uk>
2016-01-22 12:21:29 +0000

    Updated comments on menu class

--- com/amaze/main/Menu.java
+++ com/amaze/main/Menu.java
@@ -1,7 +1,7 @@
 package com.amaze.main;
 
 /**
- * This class will handle the menu and buttons associated.
+ * This class will handle the menu and buttons associated with it.
  */
 public class Menu {
 }
