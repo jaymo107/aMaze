61947f96247cf221c42646a59a29fdc45230579c
pavelsdembo <Zvenn@dyn-10-32-185-12.winet.local>
2016-01-22 11:11:35 +0000

    testing 2

--- com/amaze/main/Driver.java
+++ com/amaze/main/Driver.java
@@ -2,7 +2,7 @@ package com.amaze.main;
 import org.jsfml.system.*;
 
 /**
- * This is a test comment! Please format comments in this style! stuff
+ * This is a test comment! Please format comments in this style!
  */
 
 class Driver{
