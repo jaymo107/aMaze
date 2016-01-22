7e7e40a8eea13b3fe29719e2c9fad60b0add6fc2
Jay <jayfeng0625@gmail.com>
2016-01-22 11:26:27 +0000

    Jay

--- com/amaze/main/Driver.java
+++ com/amaze/main/Driver.java
@@ -9,7 +9,7 @@ import org.jsfml.system.*;
  *
  * KIRAN WA5 HERE
 >>>>>>> Stashed changes
- * JAY WAS HERE TOOOOOO
+ * JAY WAS HERE TOOOOOO, and he made a class.
  */
 
 class Driver{
@@ -19,7 +19,20 @@ class Driver{
         Vector3f vec1 = new Vector3f(23f, 324f, 832f);
         Vector3f vec2 = new Vector3f(12f, 22f, 349f);
         Vector3f result = Vector3f.add(vec1, vec2);
-    
+
         System.out.println("This should run without any errors if JSFML is used correctly:\n"+result.toString());
+		System.out.println(new Jay().toString());
     }
+
+	private static class Jay {
+
+		public Jay(){}
+
+		public String toString() {
+			return "Jay hates static\n";
+		}
+
+	}
+
+
 }
