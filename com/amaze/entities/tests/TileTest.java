cf541c8356111f368e7cac058f472d5b84e526d2
Tom Donnellon <jmdnnlln@live.com>
2016-01-26 12:59:55 +0000

    Adding test file to Tile class

---
+++ com/amaze/entities/tests/TileTest.java
@@ -0,0 +1,24 @@
+package com.amaze.entities.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TileTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDeltaX() throws Exception {
+
+    }
+
+    @Test
+    public void testDeltaY() throws Exception {
+
+    }
+}
