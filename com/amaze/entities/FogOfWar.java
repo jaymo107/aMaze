2e88c54f3fdf69909e2c5dce5a2a84a1866b5428
James Davies <jaymo107@gmail.com>
2016-02-02 12:26:18 +0000

    Added fog of war class

---
+++ com/amaze/entities/FogOfWar.java
@@ -0,0 +1,15 @@
+package com.amaze.entities;
+
+import org.jsfml.graphics.Image;
+
+public class FogOfWar {
+  
+  private Image fog;
+  private int size;
+  
+  
+  public void setSize(int size){
+     this.size = size;
+  }
+
+}
