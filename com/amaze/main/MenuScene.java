a04272f8a3f3772f485f956df2f9d29a1f0d8632
James Davies <jaymo107@gmail.com>
2016-02-15 23:23:38 +0000

    New main menu music

--- com/amaze/main/MenuScene.java
+++ com/amaze/main/MenuScene.java
@@ -59,7 +59,7 @@ public class MenuScene extends Scene {
         music = new Music();
         click = new Music();
         try {
-            music.openFromFile(Paths.get("res/music/gs2.wav"));
+            music.openFromFile(Paths.get("res/music/theme.wav"));
             click.openFromFile(Paths.get("res/music/Click.wav"));
         } catch (IOException e) {
             System.out.println("There was a problem loading the background music or click music.");
