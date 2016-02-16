f1b75919c16d7b632752ee8587cfd2bb967235ec
matthewmfrost <matthewmfrosts@gmail.com>
2016-02-16 17:19:32 +0000

    Added instructions button and instructions scene

--- com/amaze/main/InstructionScene.java
+++ com/amaze/main/InstructionScene.java
@@ -1,24 +1,86 @@
 package com.amaze.main;
 
-import org.jsfml.graphics.RectangleShape;
-import org.jsfml.graphics.Texture;
+import org.jsfml.graphics.*;
 import org.jsfml.system.Vector2f;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
-/**
- * Created by MatthewFrost on 16/02/16.
- */
-public class InstructionScene extends RectangleShape
-{
-    public InstructionScene(float width, float height) throws IOException
-    {
-        Vector2f size = new Vector2f(width, height);
-        Texture icon = new Texture();
-        icon.loadFromFile(Paths.get("res/menuGraphics/instructions.png"));
-        this.setTexture(icon);
-        icon.setSmooth(true);
-        this.setSize(size);
+public class InstructionScene extends Scene {
+
+    private RectangleShape textBackground;
+    private Texture backgroundImage = new Texture();
+    private Window wnd;
+
+
+    public InstructionScene(String sceneTitle, Window window) throws IOException {
+        super(sceneTitle,window);
+        wnd = window;
+
+        //Create background
+        new Background(window.getScreenWidth(), window.getScreenHeight());
+
+        //Setting size of background shader (white part)
+        Vector2f size = new Vector2f(window.getScreenWidth(), window.getScreenHeight());
+        textBackground = new RectangleShape(size);
+        textBackground.setPosition(0,0);
+
+        //Loading of Background Image for Text box
+        backgroundImage.loadFromFile(Paths.get("res/menuGraphics/instructions.png"));
+        textBackground.setTexture(backgroundImage);
+    }
+
+
+    public void arrowKeyUp() {}
+
+    public void arrowKeyDown() {}
+
+    public void enterPressed() throws Exception {}
+
+    public void executeEvent(Event event) {
+        switch(event.type) {
+            case CLOSED:
+                systemExit();
+                break;
+            case KEY_PRESSED:
+                switch (event.asKeyEvent().key) {
+                    case UP: arrowKeyUp(); break;
+                    case DOWN: arrowKeyDown(); break;
+                    case ESCAPE: exitScene(this); break;
+                    case RETURN:
+                        try {
+                            enterPressed();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            case JOYSTICK_BUTTON_PRESSED:
+
+                System.out.println(event.asJoystickButtonEvent().button);
+
+                switch (event.asJoystickButtonEvent().button) {
+
+                    case 1: arrowKeyDown();break;
+                    case 3: arrowKeyUp();break;
+                    case 12: exitScene(this); break;
+                    case 13:
+                        try {
+                            enterPressed();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+                break;
+        }
+    }
+
+    public void drawGraphics(RenderWindow window) {
+        window.draw(textBackground);
     }
 }
---
+++ com/amaze/main/InstructionsButton.java
@@ -0,0 +1,54 @@
+package com.amaze.main;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * This class holds appropriate information about play button.
+ */
+public class InstructionsButton extends Button {
+
+    Window window;
+
+    /**
+     * Construct a button with following parameters:
+     *
+     * @param xCord - x-coordinate of the button
+     * @param yCord - y-coordinate of the button
+     * @param width - width of the button
+     * @param height - height of the button
+     * @param window - reference to the main window
+     */
+    public InstructionsButton(float xCord, float yCord, float width, float height, Window window, MenuScene menu) throws IOException {
+        super(xCord, yCord, width, height, window, menu);
+
+        this.window = window;
+
+        getDefaultIcon().loadFromFile(Paths.get("res/menuGraphics/instructions.png"));
+        getSelectedIcon().loadFromFile(Paths.get("res/menuGraphics/instructionssel.png"));
+        getDefaultIcon().setSmooth(true);
+        getSelectedIcon().setSmooth(true);
+
+        this.setTexture(getDefaultIcon());
+
+    }
+
+    /**
+     * This function changes the scene to LevelMenuScene when called.
+     */
+    public void performAction() {
+
+        System.out.println("Instruction Button Pressed");
+
+        try {
+
+            InstructionScene instructionScene = new InstructionScene("Instructions", window);
+            window.addScene(instructionScene);
+            window.setScene(window.getArrayList().indexOf(instructionScene));
+            getMenu().setRunning(false);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
